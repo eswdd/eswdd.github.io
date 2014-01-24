21c90050891e41d349660aa30b7460f5fed6a52e
Simon Matic Langford <simon@exemel.co.uk>
2014-01-24 07:01:55 +0000

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TMP_DIR=/tmp/publish_$$
+mkdir -p $TMP_DIR
+cd $TMP_DIR
+
+REPO=https://github.com/eswdd/eswdd.github.io.git
+
+echo "Repository: $REPO"
+
+git clone -b master https://$REPO repo
+
+cd $TMP_DIR/repo
+bundle exec jekyll build --trace --config _config.yml
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  exit 1
+fi
+cd $TMP_DIR
+rm -rf repo
